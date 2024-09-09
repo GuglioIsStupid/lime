0ae21c24fa28f169bd979716adbb03d111838019
GuglioIsStupid <bradsbluetab@gmail.com>
2024-09-09 12:01:51 -0400

    ss

--- project/include/system/CFFI.h
+++ project/include/system/CFFI.h
@@ -3,10 +3,13 @@
 
 
 #define HL_NAME(n) lime_##n
+#if LIME_HASHLINK
 #include <hl.h>
+#endif
 #undef DEFINE_PRIM
 #define DEFINE_HL_PRIM(t, name, args) DEFINE_PRIM_WITH_NAME(t, name, args, name)
 
+#if LIME_HASHLINK
 typedef vdynamic hl_vdynamic;
 typedef vobj hl_vobj;
 typedef vvirtual hl_vvirtual;
@@ -16,6 +19,17 @@ typedef vclosure_wrapper hl_vclosure_wrapper;
 typedef vdynobj hl_vdynobj;
 typedef venum hl_venum;
 typedef vstring hl_vstring;
+#else
+typedef void hl_vdynamic;
+typedef void hl_vobj;
+typedef void hl_vvirtual;
+typedef void hl_varray;
+typedef void hl_vclosure;
+typedef void hl_vclosure_wrapper;
+typedef void hl_vdynobj;
+typedef void hl_venum;
+typedef void hl_vstring;
+#endif
 
 #undef hl_aptr
 #define hl_aptr(a,t)	((t*)(((hl_varray*)(a))+1))
